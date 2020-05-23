22fe307e88f36c1f84df9b8ee8127a98e7388490
andresperezfc <felipe3apc@gmail.com>
2020-05-23 17:34:48 -0500

    Listar personas codificado

--- app/src/main/java/com/example/personas/CrearPersonas.java
+++ app/src/main/java/com/example/personas/CrearPersonas.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CrearPersonas extends AppCompatActivity {
 
@@ -21,14 +22,16 @@ public class CrearPersonas extends AppCompatActivity {
     }
 
     public void guardar(View V){
-        String ced, nom, apell;
+        String ced, nom, apell, mensaje_guardado;
         Persona persona;
+        mensaje_guardado = getResources().getString(R.string.mensaje_guardado);
         if(validar()){
             ced = cedula.getText().toString();
             nom = nombre.getText().toString();
             apell = apellido.getText().toString();
             persona = new Persona(ced, nom, apell);
             persona.guardar();
+            Toast.makeText(this, mensaje_guardado, Toast.LENGTH_LONG).show();
         }
     }
 
